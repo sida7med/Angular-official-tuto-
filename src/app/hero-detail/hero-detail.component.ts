9d5ed31201216bcce11952051ac7f5f6662832be
ahmed <sida7medess@gmail.com>
2017-12-31 17:37:03 +0100

    Finished project

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -24,12 +24,18 @@ export class HeroDetailComponent implements OnInit {
   }
 
   getHero(): void {
-  const id = +this.route.snapshot.paramMap.get('id');
-  this.heroService.getHero(id)
-    .subscribe(hero => this.hero = hero);
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
   }
 
   goBack(): void {
     this.location.back();
   }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+
 }
